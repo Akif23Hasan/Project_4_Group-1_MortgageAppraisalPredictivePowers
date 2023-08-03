b617a9488b3fb82963760d38b3fa882d9418b47e
paak1997 <paak1997@gmail.com>
2023-08-03 21:47:22 +1000

    Yeeeet

--- app.py
+++ app.py
@@ -68,12 +68,13 @@ import traceback
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import joblib
 
 app = Flask(__name__)
 
 # Load the model and label encoder
-rf_model = None  # Load the trained RandomForestClassifier model here
-label_encoder = None  # Load the LabelEncoder used for categorical variables here
+rf_model = joblib.load('RandomForest/rf_model.pkl')  # Load the trained RandomForestClassifier model here
+label_encoder = joblib.load('RandomForest/label_encoder.pkl')  # Load the LabelEncoder used for categorical variables here
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -82,22 +83,22 @@ def index():
             user_data = {}
             for col in ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']:
                 user_data[col] = [request.form[col]]
-            user_df_1 = pd.DataFrame(user_data)
-            Loan_df = pd.read_csv("../Datasets/ETL_Completed_LoanApprovalPrediction.csv")
+
+            user_df = pd.DataFrame(user_data)
 
             # Convert categorical variables to numerical using the loaded label encoder
             for col in ['Gender', 'Married', 'Education', 'Self_Employed', 'Property_Area']:
-                user_df[col] = label_encoder.tranLoan_dfsform(user_df[col])
+                user_df[col] = label_encoder.transform(user_df[col])
 
             # Convert other numeric fields to appropriate data types
             numeric_columns = ['Dependents', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History']
             user_df[numeric_columns] = user_df[numeric_columns].apply(pd.to_numeric)
 
             # Ensure the columns in user_df match the columns used during training
-            missing_cols = set(Loan_df.columns) - set(user_df.columns)
+            missing_cols = set(X.columns) - set(user_df.columns)
             for col in missing_cols:
                 user_df[col] = 0
-            user_df = user_df[Loan_df.columns]  # Reorder the columns to match the order used during training
+            user_df = user_df[X.columns]  # Reorder the columns to match the order used during training
 
             # Make predictions
             user_pred = rf_model.predict(user_df)
