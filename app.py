f52dd4f9b034a481e0c20136850f234fc7779498
Akif23Hasan <Hasanakif96@gmail.com>
2023-08-05 20:49:20 +1000

    Jitting updated files from Saturday night session

--- app.py
+++ app.py
@@ -65,53 +65,70 @@
     #--------------------------------------------------
 
 import traceback
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
+data = pd.read_csv('Datasets/ETL_Completed_LoanApprovalPrediction.csv')
+
 app = Flask(__name__)
 
-# Load the model and label encoder
 rf_model = joblib.load('RandomForest/rf_model.pkl')  # Load the trained RandomForestClassifier model here
-label_encoder = joblib.load('RandomForest/label_encoder.pkl')  # Load the LabelEncoder used for categorical variables here
+# label_encoder = joblib.load('RandomForest/label_encoder.pkl')  # Load the LabelEncoder used for categorical variables here
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        try:
-            user_data = {}
-            for col in ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']:
-                user_data[col] = [request.form[col]]
+    return render_template('index.html')
 
-            user_df = pd.DataFrame(user_data)
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    try:
+        user_data = {}
+        form_keys = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+        for col in form_keys:
+            user_data[col] = request.form.get(col)
 
-            # Convert categorical variables to numerical using the loaded label encoder
-            for col in ['Gender', 'Married', 'Education', 'Self_Employed', 'Property_Area']:
-                user_df[col] = label_encoder.transform(user_df[col])
+        # Convert categorical variables to numerical using the loaded label encoder
+        label_encoder = LabelEncoder()
+        categorical_columns = ['Gender', 'Married', 'Education', 'Self_Employed', 'Property_Area']
+        for col in categorical_columns:
+            user_data[col] = label_encoder.fit_transform([user_data[col]])[0]
 
-            # Convert other numeric fields to appropriate data types
-            numeric_columns = ['Dependents', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History']
-            user_df[numeric_columns] = user_df[numeric_columns].apply(pd.to_numeric)
+        # Convert other numeric fields to appropriate data types
+        numeric_columns = ['Dependents', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History']
+        for col in numeric_columns:
+            if user_data[col]:
+                user_data[col] = float(user_data[col])
+            else:
+                user_data[col] = 0.0  # Set to a default value for empty fields
 
-            # Ensure the columns in user_df match the columns used during training
-            missing_cols = set(X.columns) - set(user_df.columns)
-            for col in missing_cols:
-                user_df[col] = 0
-            user_df = user_df[X.columns]  # Reorder the columns to match the order used during training
+        # Ensure the properties in user_data match the properties used during training
+        training_columns = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+        for col in training_columns:
+            if col not in user_data:
+                user_data[col] = 0
 
-            # Make predictions
-            user_pred = rf_model.predict(user_df)
-            prediction = "Approved" if user_pred[0] == 1 else "Denied"
+        # Reorder the properties to match the order used during training
+        ordered_columns = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+        user_data = {col: user_data[col] for col in ordered_columns}
 
-            return render_template('index.html', prediction=prediction)
+        # Print user_data for debugging
+        print("User Data:", user_data)
 
-        except Exception as e:
-            traceback.print_exc()  # Print the traceback for debugging
-            error_message = str(e)
-            return render_template('index.html', prediction=None, error=error_message)
+        # Make predictions
+        user_pred = rf_model.predict([list(user_data.values())])
+        prediction = "Approved" if user_pred[0] == 1 else "Denied"
+        result = {
+            'prediction': prediction,
+            'approved_probability': rf_model.predict_proba([list(user_data.values())])[0][1],  # Probability of being approved
+            'denied_probability': rf_model.predict_proba([list(user_data.values())])[0][0]      # Probability of being denied
+        }
+        return jsonify(result)
+    except Exception as e:
+        error_message = str(e)
+        return jsonify({'error': error_message})
 
-    return render_template('index.html', prediction=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
